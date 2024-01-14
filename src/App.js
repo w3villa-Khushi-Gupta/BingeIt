2d28f56eb07ca5052e4cac4f5883ba56a487143d
khushi <khushi.gupta@w3villa.com>
2024-01-14 17:13:57 +0530

    added protected routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
+// import './App.css';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Home from './component/Home';
+// import Login from './component/login-signup/Login';
+// import Register from './component/login-signup/Register';
+// import ProtectedRoute from './component/ProtectedRoute';
+// import Temp from './component/Temp';
+// function App() {
+//   return (
+//     <div className="App">
+//         <BrowserRouter>
+//           <Routes>
+//             <Route element = {<ProtectedRoute />}>
+//               <Route path='/home' element = {<Home />} />
+//               <Route path='/temp' element={<Temp /> } />
+
+//             </Route>
+//             <Route path='/login' element = {<Login />} />
+//             <Route path='/register' element = {<Register />} />
+//           </Routes>
+//         </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './component/Home';
 import Login from './component/login-signup/Login';
 import Register from './component/login-signup/Register';
 import ProtectedRoute from './component/ProtectedRoute';
+import Temp from './component/Temp';
+
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path='/login' element = {<Login />} />
-            <Route path='/register' element = {<Register />} />
-            <Route element = {<ProtectedRoute />}>
-              <Route path='/home' element = {<Home />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Use ProtectedRoute as a wrapper for protected routes */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                {/* Update the path to '/' for Home */}
+                <Route path="/" element={<Home />} />
+                <Route path="/temp" element={<Temp />} />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/ProtectedRoute.jsx
+++ src/component/ProtectedRoute.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
+import Temp from './Temp';
 
-function ProtectedRoute({ children }) {
+function ProtectedRoute() {
     const token = localStorage.getItem('token');
+    console.log(token)
+    // const navigate =useNavigate()
 
-    if (!token) {
-        return <Navigate to="/login" replace />;
-    }
+    return token ? (<Outlet />) : (<Navigate to= '/temp' replace />)
+    // if (!token) {
+    //     return 
+    // } else {
+    //     // return <Navigate to="/temp" replace />;
+    //     <Outlet />
+    // }
 
-    return children;
+    // // Move this line outside of the if-else block
+    // return children;
 }
 
 export default ProtectedRoute;
